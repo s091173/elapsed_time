e3a046c38d139f89ed34ced266929406da8861c4
Yuchi Su <astraiostor@gmail.com>
2021-01-16 16:19:54 +0800

    feat: prevent favicon request & modify variable

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const moment = require('moment')
 const port = 3000
 
+// prevent favicon request
+const icon = '<link rel="icon" href="data:,"></link>'
+
 // time 
 function timestamps(time) {
   return moment().format('YYYY-MM-DD HH:mm:ss')
@@ -19,7 +22,7 @@ app.use((req, res, next) => {
   console.log(timestamps(startTime), '|', method, 'from ', url,)
 
   res.on('finish', () => {
-    endTime = Date.now()
+    const endTime = Date.now()
     const duration = endTime - startTime
     console.log(timestamps(endTime), '|', method, 'from ', url, '|', 'total time: ', duration, 'ms')
   })
@@ -28,19 +31,19 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.send(icon + '列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.send(icon + '新增 Todo 頁面')
 })
 
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  res.send(icon + '顯示一筆 Todo')
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  res.send(icon + '新增一筆  Todo')
 })
 
 app.listen(port, () => {
